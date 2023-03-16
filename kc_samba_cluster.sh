9b0e79607f1a6e75e11c7b8847943570eef1475e
Sachin Prabhu <sprabhu@redhat.com>
2023-03-16 11:55:26 +0000

    Do not start AD server from kc_samba_cluster.sh

--- kc_samba_cluster.sh
+++ kc_samba_cluster.sh
@@ -1,13 +1,7 @@
-SMBOPERATORDIR=/home/sprabhu/dev/ocs/samba-operator
 ROOKDIR=/home/sprabhu/dev/ocs/rook
 echo minikube: Start
 #minikube start --nodes=3 --extra-disks=2 --memory 4096 --cpus 2
 ~/bin-kube/mk_start
-
-echo: Ad-server: install
-cd $SMBOPERATORDIR
-./tests/test-deploy-ad-server.sh
-cd ..
 
 echo rook-ceph: Install
 kubectl create -f $ROOKDIR/deploy/examples/crds.yaml
@@ -38,6 +32,3 @@ echo samba-operator: Install
 cd $SMBOPERATORDIR
 ./mydeploy.sh
 #make DEVELOPER=1 deploy
-
-
-
