ff7afd229c7be903e56cdba27233f415e04f1a66
Sachin Prabhu <sprabhu@redhat.com>
2023-03-16 23:32:02 +0000

    Cleanup kc_nfs_cluster.sh

--- kc_nfs_cluster.sh
+++ kc_nfs_cluster.sh
@@ -5,22 +5,30 @@ echo minikube: Start
 ~/bin-kube/mk_start
 
 echo rook-ceph: Install
-kubectl create -f $ROOKDIR/deploy/examples/crds.yaml
-kubectl create -f $ROOKDIR/deploy/examples/common.yaml
-kubectl create -f $ROOKDIR/deploy/examples/operator.yaml
-# Patch configmap to enable NFS
-kubectl -n rook-ceph patch configmap/rook-ceph-operator-config --type merge \
-	-p '{"data":{"ROOK_CSI_ENABLE_NFS":"true"}}'
-kubectl create -f $ROOKDIR/deploy/examples/cluster.yaml
-
+kubectl create -f $ROOKDIR/deploy/examples/crds.yaml -f $ROOKDIR/deploy/examples/common.yaml -f $ROOKDIR/deploy/examples/operator.yaml
 echo rook-ceph: Wait for rook-ceph pods to come up
 while kubectl -n rook-ceph get pods |grep -v ^NAME|egrep -v 'Running|Completed'; do sleep 5; done
 sleep 10;
 while kubectl -n rook-ceph get pods |grep -v ^NAME|egrep -v 'Running|Completed'; do sleep 5; done
 
-echo rook-ceph: Install required tools
+echo rook-ceph: Enable NFS
+# Patch configmap to enable NFS
+kubectl -n rook-ceph patch configmap/rook-ceph-operator-config --type merge \
+	-p '{"data":{"ROOK_CSI_ENABLE_NFS":"true"}}'
+# Patch configmap to disable block devices exported by ceph -- To reduce resource usage
+kubectl -n rook-ceph patch configmap/rook-ceph-operator-config --type merge \
+	-p '{"data":{"ROOK_CSI_ENABLE_RBD":"false"}}'
+# Start up cluster
+kubectl create -f $ROOKDIR/deploy/examples/cluster.yaml
+echo rook-ceph: Wait for rook-ceph pods to come up
+while kubectl -n rook-ceph get pods |grep -v ^NAME|egrep -v 'Running|Completed'; do sleep 5; done
+sleep 10;
+while kubectl -n rook-ceph get pods |grep -v ^NAME|egrep -v 'Running|Completed'; do sleep 5; done
+
+echo rook-ceph: Install tool box. This is the container with tools required to manipulate ceph
 kubectl create -f $ROOKDIR/deploy/examples/toolbox.yaml
-kubectl create -f $ROOKDIR/deploy/examples/pool.yaml
+
+# Create a filesystem - This filesystem will be exported by NFS
 kubectl create -f $ROOKDIR/deploy/examples/filesystem.yaml
 
 echo rook-ceph: Wait for rook-ceph toolbox pods to come up
@@ -28,6 +36,7 @@ while kubectl -n rook-ceph get pods |grep -v ^NAME|egrep -v 'Running|Completed';
 sleep 10;
 while kubectl -n rook-ceph get pods |grep -v ^NAME|egrep -v 'Running|Completed'; do sleep 5; done
 
+# Other ceph stuff to set cephfs as default for storageclass
 echo rook-ceph: Setup cephfs storage class
 kubectl apply -f $ROOKDIR/deploy/examples/csi/cephfs/storageclass.yaml
 kubectl patch storageclass rook-cephfs -p '{"metadata": {"annotations":{"storageclass.kubernetes.io/is-default-class":"true"}}}'
@@ -37,20 +46,23 @@ echo
 kubectl get storageclass
 echo
 
-# https://rook.github.io/docs/rook/v1.9/ceph-nfs-crd.html#ceph-csi-nfs-provisioner-and-nfs-csi-driver
-echo rook-ceph: enable nfs
-TOOLBOX_CONTAINER=$(kubectl -n rook-ceph get pod -l "app=rook-ceph-tools" -o jsonpath='{.items[0].metadata.name}')
-#kubectl -n rook-ceph exec -it $TOOLBOX_CONTAINER -- ceph mgr module enable rook
-#kubectl -n rook-ceph exec -it $TOOLBOX_CONTAINER -- ceph mgr module enable nfs
-#kubectl -n rook-ceph exec -it $TOOLBOX_CONTAINER -- ceph orch set backend rook
-echo
-
-echo rook-ceph: Setup ceph-nfs
-kubectl apply -f $ROOKDIR/deploy/examples/csi/nfs/rbac.yaml
+echo rook-ceph: setup cephnfs sample.
+# Create CephNFS resource - my-nfs from rook samples
 kubectl create -f $ROOKDIR/deploy/examples/nfs.yaml
-kubectl apply -f $ROOKDIR/deploy/examples/csi/nfs/storageclass.yaml
+
+#short delay to allow ceph-nfs to comeup
+sleep 10
+# Create the nfs export
+kr_ceph_tools ceph nfs export create cephfs my-nfs /myfs myfs
+
+
+#TOOLBOX_CONTAINER=$(kubectl -n rook-ceph get pod -l "app=rook-ceph-tools" -o jsonpath='{.items[0].metadata.name}')
+
+#echo rook-ceph: Setup csi nfs
+# kubectl apply -f $ROOKDIR/deploy/examples/csi/nfs/rbac.yaml
+# kubectl apply -f $ROOKDIR/deploy/examples/csi/nfs/storageclass.yaml
+#echo rook-nfs: Setup demo
+# kubectl apply -f $ROOKDIR/deploy/examples/csi/nfs/pvc.yaml
+#kubectl apply -f $ROOKDIR/deploy/examples/csi/nfs/pod.yaml
 
 # https://rook.github.io/docs/rook/v1.9/ceph-nfs-crd.html#creating-exports
-echo rook-nfs: Setup demo
-kubectl apply -f $ROOKDIR/deploy/examples/csi/nfs/pvc.yaml
-kubectl apply -f $ROOKDIR/deploy/examples/csi/nfs/pod.yaml
